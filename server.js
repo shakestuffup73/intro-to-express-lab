69167457e9daca8bbe1879de05b3500b72a6da8e
Amanda Stern <amanda.stern.73@gmail.com>
2022-09-19 17:15:29 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+// import modules
+
+import express from 'express'
+
+
+
+// create express app
+
+const app = express()
+
+// configure the app (app.set)
+
+
+
+// Mount middleware
+
+
+
+// Mount routes
+app.get('/', function(req, res){
+  res.send('<h1>hello, friend</h1>')
+})
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('listening on port 3000')
+})
